d91f48148021240cf83369ac563e1279dbf70bcf
GrayrayMiao123 <imilkris@163.com>
2024-12-20 17:38:02 -0500

    add compressed yolov10 model

---
+++ model_compression/README.md
@@ -0,0 +1,16 @@
+## Acknowledgement
+
+The code base is built with [ultralytics](https://github.com/ultralytics/ultralytics), [RT-DETR](https://github.com/lyuwenyu/RT-DETR) and GuoquanPei(https://github.com/GuoquanPei/YOLOv8-MobileNetv4-ultralytics.git)
+
+Thanks for their great implementations! 
+
+## Citation
+
+```BibTeX
+@article{wang2024yolov10,
+  title={YOLOv10: Real-Time End-to-End Object Detection},
+  author={Wang, Ao and Chen, Hui and Liu, Lihao and Chen, Kai and Lin, Zijia and Han, Jungong and Ding, Guiguang},
+  journal={arXiv preprint arXiv:2405.14458},
+  year={2024}
+}
+```
---
+++ model_compression/test.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+
+model = YOLO('/mnt/workspace/NYU-CV-Fall2024-Project/yolov10/runs/prune/n-yolov8-nop234-gam-finetune2/weights/best.pt')
+
+# Validate the model
+results = model.val(
+    data="/mnt/workspace/yolov10/ultralytics/cfg/datasets/TT100K.yaml",
+    imgsz=640,
+    project='runs/prune/validation',
+    name='test1'
+)
+
+# 获取详细的评估指标
+metrics = results.results_dict
+
+# 打印主要指标
+print(f"\nValidation Results:")
+print(f"mAP50: {metrics['metrics/mAP50(B)']:.5f}")
+print(f"mAP50-95: {metrics['metrics/mAP50-95(B)']:.5f}")
+print(f"Precision: {metrics['metrics/precision(B)']:.5f}")
+print(f"Recall: {metrics['metrics/recall(B)']:.5f}")
+
+# 如果需要保存这些指标到文件
+with open('runs/my_validation/test1/metrics.txt', 'w') as f:
+    f.write(f"mAP50: {metrics['metrics/mAP50(B)']:.5f}\n")
+    f.write(f"mAP50-95: {metrics['metrics/mAP50-95(B)']:.5f}\n")
+    f.write(f"Precision: {metrics['metrics/precision(B)']:.5f}\n")
+    f.write(f"Recall: {metrics['metrics/recall(B)']:.5f}\n")
